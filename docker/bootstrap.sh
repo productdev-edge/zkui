16c576e2f52e3aedb20a5659bd61cee74daf7f9b
ayush28j <jain.ayushjain.ayush9@gmail.com>
2023-12-28 12:19:17 +0530

    Deploying docker without MakeFile

--- docker/bootstrap.sh
+++ docker/bootstrap.sh
@@ -2,14 +2,6 @@
 
 ZK_SERVER=${ZK_SERVER:-"localhost:2181"}
 
-USER_SET=${USER_SET:-"{\"users\": [{ \"username\":\"admin\" , \"password\":\"manager\",\"role\": \"ADMIN\" \},{ \"username\":\"appconfig\" , \"password\":\"appconfig\",\"role\": \"USER\" \}]\}"}
-LOGIN_MESSAGE=${LOGIN_MESSAGE:-"Please login using admin/manager or appconfig/appconfig."}
-
-sed -i "s/^zkServer=.*$/zkServer=$ZK_SERVER/" /var/app/config.cfg
-
-sed -i "s/^userSet = .*$/userSet = $USER_SET/" /var/app/config.cfg
-sed -i "s/^loginMessage=.*$/loginMessage=$LOGIN_MESSAGE/" /var/app/config.cfg
-
-echo "Starting zkui with server $ZK_SERVER"
+echo "Starting zkui with server $ZK_SERVER at port 9090"
 
 exec java -jar /var/app/zkui.jar
